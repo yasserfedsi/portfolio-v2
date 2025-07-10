18ca7aaf2c0202ff9763e966f289326d1fc63554
Yasser <yasserfedsi34@gmail.com>
2025-07-10 17:44:32 +0200

    Refactor Home component: update structure and remove commented code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Hero from "@/components/Home/Hero/page";
 import Features from "@/components/Home/Features/page";
-import VerticalSidebar from "@/components/Sidebar/page";
+import Projects from "@/components/Home/Projects/page";
+import Footer from "@/components/Home/Footer/page";
 
 export default function Home() {
   return (
-    <>
+    <main className="overflow-y-scroll h-screen">
       <Hero />
-      {/*       <Features />
-       */}{" "}
-    </>
+      <Features />
+      <Projects />
+      <Footer />
+    </main>
   );
 }
